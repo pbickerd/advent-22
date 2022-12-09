d37c2f096e0b870175112d9d3ee55927c8787f1e
Paul Bickerdyke <Paul.Bickerdyke@frasers.group>
2022-12-09 16:49:19 +0000

    add task 3

--- 3/main.py
+++ 3/main.py
@@ -6,7 +6,7 @@ total = 0
 alphabet = list(string.ascii_letters)
 
 def getValue(commonCharacter):
-    character = list(commonCharacter)[0] # Item comes in a list, need to get it into a string
+    character = list(commonCharacter)[0] # Item comes in a set, need to get it into a string
     score = alphabet.index(character) + 1 # Get the index value of the character passed in and add 1 as our index starts at 0
     return score
 
@@ -15,6 +15,6 @@ with open("input.txt") as file:
         firsthalf = set(line[:len(line)//2]) # take first half of the string and put in a set
         secondhalf = set(line[len(line)//2:]) # take second half of the string and put in a set
         common = firsthalf & secondhalf # Set intersection gives us the common characters
-        total = total + getValue(common) # User our function that returns a score
+        total = total + getValue(common) # Use our function that returns a score
 
 print("The total is ", total)
